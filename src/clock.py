bfd52f898b6440041ba491fee192d2232dbc5fbe
Peter Morgan <smartattack@gmail.com>
2018-07-12 22:14:10 -0400

    Add clock.py to contain time routines

---
+++ src/clock.py
@@ -0,0 +1,21 @@
+'''
+Manage game time
+'''
+
+import time
+from utils import log
+import globals as GLOBALS
+
+
+def start_clock():
+    '''Start game time, note time of boot'''
+    GLOBALS.EPOCH_S = int(time.strftime('%s', time.strptime(GLOBALS.GAME_EPOCH, '%Y/%m/%d %H:%M:%S')))
+    log.info('Converted GAME_EPOCH: %s -> %s', GLOBALS.GAME_EPOCH, GLOBALS.EPOCH_S)
+    log.info('Game time will be scaled by a factor of: %s', GLOBALS.TIME_FACTOR)
+    GLOBALS.boot_time = int(time.time())
+    GLOBALS.last_update = GLOBALS.boot_time
+
+
+def now():
+    '''Return current realtime clock in seconds'''
+    return time.time()

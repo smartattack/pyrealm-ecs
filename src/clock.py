4ea6e944fdf23c8f138204c75f7965423e4b804c
Peter Morgan <smartattack@gmail.com>
2018-07-12 22:26:03 -0400

    create clock.py to handle game timekeeping

--- src/clock.py
+++ src/clock.py
@@ -7,11 +7,11 @@ from utils import log
 import globals as GLOBALS
 
 
-def start_clock():
+def start_clock(time_factor=24):
     '''Start game time, note time of boot'''
     GLOBALS.EPOCH_S = int(time.strftime('%s', time.strptime(GLOBALS.GAME_EPOCH, '%Y/%m/%d %H:%M:%S')))
     log.info('Converted GAME_EPOCH: %s -> %s', GLOBALS.GAME_EPOCH, GLOBALS.EPOCH_S)
-    log.info('Game time will be scaled by a factor of: %s', GLOBALS.TIME_FACTOR)
+    log.info('Game time will be scaled by a factor of: %s', time_factor)
     GLOBALS.boot_time = int(time.time())
     GLOBALS.last_update = GLOBALS.boot_time
 
